eaefa63f5f78aedeb0157826949f6dd7353c5400
Taeyangkimmmmm <95173691+Taeyangkimmmmm@users.noreply.github.com>
2021-11-30 01:00:07 +0900

    Add files via upload

---
+++ 결과물3조_러닝페어_최종.py
@@ -0,0 +1,176 @@
+print("<2022학년도 상명대학교 역사콘텐츠전공 교과우수전형>")
+print("내신등급 및 합불 가능성을 산출해 드리는 프로그램입니다.")
+print("석차등급 칸에 1학년 1학기~3학년 1학기 각 과목별 석차등급을 입력해주세요.")
+print("이수단위 칸에 해당 교과목의 단위시간을 입력해주세요.")
+print("진로우수 선택과목의 성취등급의 경우 입력하는 방식 : ")
+print("A등급 = 1")
+print("B등급 = 3")
+print("C등급 = 5")
+print("(A, B, C 등급을 각각 1, 3, 5의 성취도별 환산 등급으로 측정)")
+
+
+
+
+print("")
+print("1학년 1학기")
+
+a1=float(input("국어 석차등급 : "))
+b1=int(input("국어 이수시간 : "))
+
+c1=float(input("영어 석차등급 : "))
+d1=int(input("영어 이수시간 : "))
+
+e1=float(input("수학 석차등급 : "))
+f1=int(input("수학 이수시간 : "))
+
+g1=float(input("사회 석차등급 : "))
+h1=int(input("사회 이수시간 : "))
+
+i1=float(input("진로우수선택과목1 성취등급 : "))
+j1=int(input("진로우수선택과목1 이수시간 : "))
+
+k1=float(input("진로우수선택과목2 성취등급 : "))
+l1=int(input("진로우수선택과목2 이수시간 : "))
+
+m1=float(input("진로우수선택과목3 성취등급 : "))
+n1=int(input("진로우수선택과목3 이수시간 : "))
+
+agv1=((a1*b1+c1*d1+e1*f1+g1*h1+i1*j1+k1*l1+m1*n1)/(b1+d1+f1+h1+j1+l1+n1))
+
+
+print("")
+print("1학년 2학기")
+
+a2=float(input("국어 석차등급 : "))
+b2=int(input("국어 이수시간 : "))
+
+c2=float(input("영어 석차등급 : "))
+d2=int(input("영어 이수시간 : "))
+
+e2=float(input("수학 석차등급 : "))
+f2=int(input("수학 이수시간 : "))
+
+g2=float(input("사회 석차등급 : "))
+h2=int(input("사회 이수시간 : "))
+
+i2=float(input("진로우수선택과목1 성취등급 : "))
+j2=int(input("진로우수선택과목1 이수시간 : "))
+
+k2=float(input("진로우수선택과목2 성취등급 : "))
+l2=int(input("진로우수선택과목2 이수시간 : "))
+
+m2=float(input("진로우수선택과목3 성취등급 : "))
+n2=int(input("진로우수선택과목3 이수시간 : "))
+
+agv2=((a2*b2+c2*d2+e2*f2+g2*h2+i2*j2+k2*l2+m2*n2)/(b2+d2+f2+h2+j2+l2+n2))
+
+
+
+print("")
+print("2학년 1학기")
+
+a3=float(input("국어 석차등급 : "))
+b3=int(input("국어 이수시간 : "))
+
+c3=float(input("수학 석차등급 : "))
+d3=int(input("수학 이수시간 : "))
+
+e3=float(input("영어 석차등급 : "))
+f3=int(input("영어 이수시간 : "))
+
+g3=float(input("사회 석차등급 : "))
+h3=int(input("사회 이수시간 : "))
+
+i3=float(input("진로우수선택과목1 성취등급 : "))
+j3=int(input("진로우수선택과목1 이수시간 : "))
+
+k3=float(input("진로우수선택과목2 성취등급 : "))
+l3=int(input("진로우수선택과목2 이수시간 : "))
+
+m3=float(input("진로우수선택과목3 성취등급 : "))
+n3=int(input("진로우수선택과목3 이수시간 : "))
+
+agv3=((a3*b3+c3*d3+e3*f3+g3*h3+i3*j3+k3*l3+m3*n3)/(b3+d3+f3+h3+j3+l3+n3))
+
+
+
+print("")
+print("2학년 2학기")
+
+a4=float(input("국어 석차등급 : "))
+b4=int(input("국어 이수시간 : "))
+
+c4=float(input("영어 석차등급 : "))
+d4=int(input("영어 이수시간 : "))
+
+e4=int(input("수학 석차등급 : "))
+f4=int(input("수학 이수시간 : "))
+
+g4=float(input("사회 석차등급 : "))
+h4=int(input("사회 이수시간 : "))
+
+i4=float(input("진로우수선택과목1 성취등급 : "))
+j4=int(input("진로우수선택과목1 이수시간 : "))
+
+k4=float(input("진로우수선택과목2 성취등급 : "))
+l4=int(input("진로우수선택과목2 이수시간 : "))
+
+m4=float(input("진로우수선택과목3 성취등급 : "))
+n4=int(input("진로우수선택과목3 이수시간 : "))
+
+agv4=((a4*b4+c4*d4+e4*f4+g4*h4+i4*j4+k4*l4+m4*n4)/(b4+d4+f4+h4+j4+l4+n4))
+
+
+
+print("")
+print("3학년 1학기")
+
+a5=float(input("국어 석차등급 : "))
+b5=int(input("국어 이수시간 : "))
+
+c5=float(input("영어 석차등급 : "))
+d5=int(input("영어 이수시간 : "))
+
+e5=float(input("수학 석차등급 : "))
+f5=int(input("수학 이수시간 : "))
+
+g5=float(input("사회 석차등급 : "))
+h5=int(input("사회 이수시간 : "))
+
+i5=float(input("진로우수선택과목1 성취등급 : "))
+j5=int(input("진로우수선택과목1 이수시간 : "))
+
+k5=float(input("진로우수선택과목2 성취등급 : "))
+l5=int(input("진로우수선택과목2 이수시간 : "))
+
+m5=float(input("진로우수선택과목3 성취등급 : "))
+n5=int(input("진로우수선택과목3 이수시간 : "))
+
+agv5=((a5*b5+c5*d5+e5*f5+g5*h5+i5*j5+k5*l5+m5*n5)/(b5+d5+f5+h5+j5+l5+n5))
+
+
+
+
+
+
+
+GR=(agv1+agv2+agv3+agv4+agv5)/5
+
+
+print("")
+print("내신등급 : ", GR)
+
+if GR <= 2.23 :
+     print("안정")
+     
+elif 2.23 < GR <= 2.59 :
+     print("적정")
+     
+else :
+     print("위험")
+
+
+
+
+
+
